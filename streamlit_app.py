a5f2bd5b612ef0f69245217389a2f994b7816708
cldbiffi <claudio1.biffi@mail.polimi.it>
2025-03-27 15:59:45 +0000

    modify 1

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,10 +1,15 @@
 import streamlit as st
 
+st.set_page_config(
+    page_title='My application for data',
+    layout='wide',
+    initial_sidebar_state='expanded'
+)
+
 st.title("🎈 My new appplication for data visialization")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
 
 value=st.slider('select a value: ', 0, 100, 30)
-
 st.write('you selected: ',value)
