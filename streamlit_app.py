f48a3a33759796298e3f172f6e416323a88e386f
cldbiffi <claudio1.biffi@mail.polimi.it>
2025-03-27 15:45:27 +0000

    data modification hhh

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,10 @@
 import streamlit as st
 
-st.title("🎈 My new app")
+st.title("🎈 My new appplication for data visialization")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
+
+value=st.slider('select a value: ', 0, 100, 30)
+
+st.write('you selected: ',value)
